aa1aecca16440b28f27a85045873f0d18c7f770a
Cedric <cedric.debot@gmail.com>
2023-01-21 16:50:32 +0100

    feat: Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Main } from './pages/Main';
+import { Login } from './pages/Login';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/login" element={<Login />}></Route>  
+        </Routes>
+      </Router>
     </div>
   );
 }
